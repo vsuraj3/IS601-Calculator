257ccf0a9ce1ff696f62a75b4fef09efa16a82f6
suraj vishwakarma <vishwakarmasuraj0@gmail.com>
2020-11-01 16:32:58 -0500

    Feature : Multiply test case

--- src/calculatorTest.py
+++ src/calculatorTest.py
@@ -27,6 +27,11 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
+     def test_multiply_method(self):
+        test_data = CsvReader('/src/csv_files/multiplication.csv').data
+        for row in test_data:
+            self.assertEqual(self.calculator.multiply(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
             
 if __name__ == '__main__':
     unittest.main()
