abdeeff2fad3d9a75d99d078600c7541bf4099f6
Suraj Vishwakarma <vishwakarmasuraj0@gmail.com>
2020-10-31 22:27:52 -0400

    Feature : Setup calculator class and test

--- src/calculatorTest.py
+++ src/calculatorTest.py
@@ -1,9 +1,14 @@
 import unittest
+from Calculator import Calculator
+from pprint import pprint
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+
+    def test_instantiate_calculator(self):
+        calculator = Calculator()
+        self.assertIsInstance(self.calculator, Calculator)
+
 
 
 if __name__ == '__main__':
