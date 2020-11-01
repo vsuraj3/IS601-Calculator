ff61c45a7ef6ab938d8f9199634cb977b7b4d803
suraj vishwakarma <vishwakarmasuraj0@gmail.com>
2020-11-01 16:23:33 -0500

    Task : update code

--- src/Calculator.py
+++ src/Calculator.py
@@ -4,24 +4,7 @@ def addition(a,b):
     c = a + b
     return c
 
-def subtract(a,b):
-    a = int(a)
-    b = int(b)
-    c = b - a
-    return c
 
-def multiplication(a, b):
-    a = int(a)
-    b = int(b)
-    c = b * a
-    return c
-
-def division(a, b):
-    a = float(a)
-    b = float(b)
-    c = b / a
-    d = round(c, 9)
-    return d
 
 class Calculator:
     result = 0
@@ -31,19 +14,4 @@ class Calculator:
 
     def add(self,a,b):
         self.result=addition(a,b)
-        return self.result
-
-    def subtract(self,a,b):
-        self.result = subtract(a, b)
-        return self.result
-    
-    def multiply(self, a, b):
-        self.result = multiplication(a, b)
-        return self.result
-    
-    def div(self, a, b):
-        self.result = division(a, b)
-        return self.result
-    def div(self, a, b):
-        self.result = division(a, b)
         return self.result
