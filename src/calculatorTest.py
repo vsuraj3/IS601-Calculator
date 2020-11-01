e93de072f65f0d338a3824039351dda6fab4db89
suraj vishwakarma <vishwakarmasuraj0@gmail.com>
2020-11-01 00:05:51 -0400

    Feature : Addition csv change

--- src/calculatorTest.py
+++ src/calculatorTest.py
@@ -16,8 +16,10 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.calculator.result, 0)
 
     def test_add_method(self):
-        self.assertEqual(self.calculator.add(2,2),4)
-        self.assertEqual(self.calculator.result,4)
+       test_data = CsvReader('/src/csv_files/addition.csv').data
+        for row in test_data:
+            self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_subtract_method(self):
         test_data = CsvReader('/src/csv_files/subtraction.csv').data
