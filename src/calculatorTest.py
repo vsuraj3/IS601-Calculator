b7aa86e4d44dee2d3a50b0b718323559ea283aa0
suraj vishwakarma <vishwakarmasuraj0@gmail.com>
2020-11-01 16:24:18 -0500

    Task : update code

--- src/calculatorTest.py
+++ src/calculatorTest.py
@@ -20,12 +20,6 @@ class MyTestCase(unittest.TestCase):
         for row in test_data:
             self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
-
-    def test_subtract_method(self):
-        test_data = CsvReader('/src/csv_files/subtraction.csv').data
-        for row in test_data:
-            self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
-            self.assertEqual(self.calculator.result, int(row['Result']))
-
+            
 if __name__ == '__main__':
     unittest.main()
